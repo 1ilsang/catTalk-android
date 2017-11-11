986d58ba28e811d2a363bf7e19c2278f7cc9feda
dongjunes <june1214>
2017-11-11 20:04:42 +0900

    .

---
+++ app/src/main/java/cattalk/depromeet/dongjune/cattalk_android/intro_viewpager/IntroViewActivity.java
@@ -0,0 +1,19 @@
+package cattalk.depromeet.dongjune.cattalk_android.intro_viewpager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cattalk.depromeet.dongjune.cattalk_android.R;
+
+public class IntroViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro_view);
+
+
+
+
+    }
+}
