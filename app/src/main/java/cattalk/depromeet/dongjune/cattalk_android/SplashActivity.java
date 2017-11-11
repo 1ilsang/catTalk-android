63224e665e402d48daec37b7f6a789c878a1cd93
dongjunes <melfs363@gmail.com>
2017-11-12 01:27:44 +0900

    .

--- app/src/main/java/cattalk/depromeet/dongjune/cattalk_android/SplashActivity.java
+++ app/src/main/java/cattalk/depromeet/dongjune/cattalk_android/SplashActivity.java
@@ -1,27 +1,40 @@
 package cattalk.depromeet.dongjune.cattalk_android;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import cattalk.depromeet.dongjune.cattalk_android.intro_viewpager.IntroViewActivity;
+
 public class SplashActivity extends AppCompatActivity {
-    private final int SPLASH_DISPLAY_LENGTH = 5000;
+    private final int SPLASH_DISPLAY_LENGTH = 2500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        new Handler().postDelayed(new Runnable(){
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 /* 메뉴액티비티를 실행하고 로딩화면을 죽인다.*/
-                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
-                SplashActivity.this.startActivity(mainIntent);
+                Intent intent;
+                if (firstCheck()) {
+                    intent = new Intent(SplashActivity.this, MainActivity.class);
+                } else {
+                    intent = new Intent(SplashActivity.this, IntroViewActivity.class);
+                }
+                SplashActivity.this.startActivity(intent);
                 SplashActivity.this.finish();
             }
         }, SPLASH_DISPLAY_LENGTH);
+    }
+
+    private boolean firstCheck() {
+        SharedPreferences sp = getPreferences(MODE_PRIVATE);
+        return sp.getBoolean("first_check", false);
     }
 
 
--- app/src/main/java/cattalk/depromeet/dongjune/cattalk_android/intro_viewpager/IntroViewActivity.java
+++ app/src/main/java/cattalk/depromeet/dongjune/cattalk_android/intro_viewpager/IntroViewActivity.java
@@ -1,9 +1,13 @@
 package cattalk.depromeet.dongjune.cattalk_android.intro_viewpager;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
+import cattalk.depromeet.dongjune.cattalk_android.MainActivity;
 import cattalk.depromeet.dongjune.cattalk_android.R;
 import cattalk.depromeet.dongjune.cattalk_android.databinding.ActivityIntroViewBinding;
 
@@ -15,10 +19,27 @@ public class IntroViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_intro_view);
+
+        saveFirstCheck();
+
         IntroViewAdapter adapter = new IntroViewAdapter(getApplicationContext(), getLayoutInflater());
         binding.introViewpager.setAdapter(adapter);
         binding.tablayout.setupWithViewPager(binding.introViewpager, true);
 
+        binding.btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(IntroViewActivity.this, MainActivity.class);
+                startActivity(intent);
+                IntroViewActivity.this.finish();
+            }
+        });
 
+    }
+
+    private void saveFirstCheck() {
+        SharedPreferences sp = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putBoolean("first_check", true);
     }
 }
