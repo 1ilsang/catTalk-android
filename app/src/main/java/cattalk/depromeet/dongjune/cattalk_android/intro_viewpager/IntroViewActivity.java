e9539c287287fe954e56e55a36f000736e840d5c
dongjunes <june1214>
2017-11-11 20:30:33 +0900

    .

--- app/src/main/java/cattalk/depromeet/dongjune/cattalk_android/intro_viewpager/IntroViewActivity.java
+++ app/src/main/java/cattalk/depromeet/dongjune/cattalk_android/intro_viewpager/IntroViewActivity.java
@@ -1,18 +1,23 @@
 package cattalk.depromeet.dongjune.cattalk_android.intro_viewpager;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import cattalk.depromeet.dongjune.cattalk_android.R;
+import cattalk.depromeet.dongjune.cattalk_android.databinding.ActivityIntroViewBinding;
 
 public class IntroViewActivity extends AppCompatActivity {
+
+    ActivityIntroViewBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_intro_view);
-
-
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_intro_view);
+        IntroViewAdapter adapter = new IntroViewAdapter(getApplicationContext(), getLayoutInflater());
+        binding.introViewpager.setAdapter(adapter);
+        binding.tablayout.setupWithViewPager(binding.introViewpager, true);
 
 
     }
